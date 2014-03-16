19670c44e35876629c2696472b5936a4f8b3dc68
Ciaran O'Neill <oneillci@gmail.com>
2014-03-16 22:04:12 +1100

    added powershell script and install cmd

---
+++ install.cmd
@@ -0,0 +1,3 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin 
+ 
+@PowerShell -ExecutionPolicy bypass -file install_apps.ps1
---
+++ install_apps.ps1
@@ -0,0 +1,51 @@
+# Dev apps to install
+
+
+# Visual studio stuff
+cinstm DotNet4.5
+#cinstm VisualStudio2013Ultimate
+#cinstm VisualStudio2012Ultimate
+
+
+# Visual studio add-ins
+#cinstm xUnit.vs2012
+#cinstm WebEssentials2012
+#cinstm resharper -Version 7.1.3000.2254 # Use only resharper if you want the latests. This is what I have a license for
+#cinstm typescript
+#cinstm typescript.vs
+#cinstm SpecFlow
+cinstm SourceCodePro
+
+# databases and tools
+#cinstm MsSqlServer2012Express
+#cinstm mongodb
+#cinstm MongoVUE
+
+
+# browsers
+cinstm GoogleChrome
+
+
+# Other tools
+#cinstm git
+#cinstm gitextensions
+cinstm githubforwindows
+cinstm SourceTree
+cinstm notepadplusplus
+cinstm winmerge
+cinstm Console2
+cinstm 7zip
+#cinstm nodejs.install
+cinstm fiddler
+cinstm curl
+cinstm linqpad4
+cinstm scriptcs
+#cinstm sublimetext2
+#cinstm SublimeText2.PackageControl
+#cinstm ccleaner
+cinstm windirstat
+
+cinstm picasa
+cinstm skype
+cinstm SkyDrive
+
