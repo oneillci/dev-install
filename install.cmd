0d7fd84cf45e7487a2c0a6a8107646418990da0f
Ciaran O'Neill <oneillci@gmail.com>
2014-03-16 22:11:15 +1100

    updated install.cmd to download from github

--- install.cmd
+++ install.cmd
@@ -1,3 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin 
  
+rem @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/oneillci/dev-install/master/install_apps.ps1'))" 
+
 @PowerShell -ExecutionPolicy bypass -file install_apps.ps1
